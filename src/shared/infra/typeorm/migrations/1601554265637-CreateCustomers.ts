ab271d7a436e6609f9a03de187b0a9f904d1934b
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-10-01 13:58:21 +0100

    gorestaurant

---
+++ src/shared/infra/typeorm/migrations/1601554265637-CreateCustomers.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCustomers1601554265637
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
